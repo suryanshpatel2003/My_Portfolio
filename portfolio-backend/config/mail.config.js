13ebd5224be1735ebd55d74e07fc30535f3ac3a0
Suryansh Patel <suryansh30patel@gmail.com>
2026-03-29 19:29:18 +0530

    fix smtp port

--- portfolio-backend/config/mail.config.js
+++ portfolio-backend/config/mail.config.js
@@ -1,15 +1,12 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
-dotenv.config(); // IMPORTANT: load env here also
-
-console.log("MAIL USER:", process.env.EMAIL_USER);
-console.log("MAIL PASS:", process.env.EMAIL_PASS);
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,        // 🔥 CHANGE HERE
+  secure: true,     // 🔥 CHANGE HERE
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
