b6402af85d837c9d7ab5a91042f54682798fbb1d
Suryansh Patel <suryansh30patel@gmail.com>
2026-03-29 19:55:51 +0530

    fixed email

--- portfolio-backend/config/mail.config.js
+++ portfolio-backend/config/mail.config.js
@@ -1,14 +1,29 @@
-import dotenv from "dotenv";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-dotenv.config();
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,        // 🔥 CHANGE HERE
-  secure: true,     // 🔥 CHANGE HERE
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+export const sendContact = async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>", // sender
+      to: "suryansh30patel@gmail.com", // tera email
+      reply_to: email, // 🔥 IMPORTANT (user ka email)
+      subject: `New Message from ${name}`,
+      html: `
+        <h2>New Contact Form Message</h2>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b></p>
+        <p>${message}</p>
+      `,
+    });
+
+    res.status(200).json({ success: true });
+
+  } catch (error) {
+    console.log("RESEND ERROR:", error);
+    res.status(500).json({ success: false });
+  }
+};
